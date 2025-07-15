0514f6ae3a572c81d16c7d68863933e1deea9c16
Lakshitha Madumal <75256642+lakshithamadumal@users.noreply.github.com>
2025-07-15 10:02:14 +0530

    Create User.java

---
+++ src/java/hibernate/User.java
@@ -0,0 +1,79 @@
+package hibernate;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ *
+ * @author Laky
+ */
+public class User implements Serializable {
+
+    int id;
+    String first_name;
+    String last_name;
+    String email;
+    String password;
+    String verification;
+    Date created_at;
+
+    public User() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getVerification() {
+        return verification;
+    }
+
+    public void setVerification(String verification) {
+        this.verification = verification;
+    }
+
+    public Date getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(Date created_at) {
+        this.created_at = created_at;
+    }
+
+}
