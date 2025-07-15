92787acb2b390e32ed9038d66d7e7dcb2b4f68bc
Lakshitha Madumal <75256642+lakshithamadumal@users.noreply.github.com>
2025-07-15 10:38:51 +0530

    signup id

---
+++ src/java/controller/SignUp.java
@@ -0,0 +1,22 @@
+package controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Laky
+ */
+@WebServlet(name = "SignUp", urlPatterns = {"/SignUp"})
+public class SignUp extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+    }
+}
---
+++ web/assets/js/sign-up.js
@@ -0,0 +1,11 @@
+function SignUp(){
+    const firstName = document.getElementById("firstName").value;
+    const lastName = document.getElementById("lastName").value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    console.log(firstName);
+    console.log(lastName);
+    console.log(email);
+    console.log(password);
+}
