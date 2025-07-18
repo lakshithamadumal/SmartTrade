aa44b3521566d680967977bf8b99b596183d0b83
Lakshitha Madumal <75256642+lakshithamadumal@users.noreply.github.com>
2025-07-18 11:01:24 +0530

    entity

--- src/java/controller/LoadProductData.java
+++ src/java/controller/LoadProductData.java
@@ -5,6 +5,7 @@ import hibernate.Color;
 import hibernate.HibernateUtil;
 import hibernate.Model;
 import hibernate.Quality;
+import hibernate.Status;
 import hibernate.Storage;
 import java.io.IOException;
 import java.util.List;
@@ -51,32 +52,6 @@ public class LoadProductData extends HttpServlet {
         Criteria c5 = s.createCriteria(Storage.class);
         List<Storage> storagelList = c5.list();
 
-        for (Brand brand : brandList) {
-            System.out.println(brand.getId());
-            System.out.println(brand.getName());
-        }
-
-        for (Model model : modelList) {
-            System.out.println(model.getId());
-            System.out.println(model.getName());
-            System.out.println(model.getBrand().getId());
-            System.out.println(model.getBrand().getName());
-        }
-
-        for (Color color : colorlList) {
-            System.out.println(color.getId());
-            System.out.println(color.getName());
-        }
-
-        for (Quality quality : qualitylList) {
-            System.out.println(quality.getId());
-            System.out.println(quality.getName());
-        }
-
-        for (Storage storage : storagelList) {
-            System.out.println(storage.getId());
-            System.out.println(storage.getName());
-        }
     }
 
 }
