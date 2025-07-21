2045aad0ed4fa127c0410610961731a30b6a4935
Lakshitha Madumal <75256642+lakshithamadumal@users.noreply.github.com>
2025-07-21 13:13:44 +0530

    load product

--- web/assets/js/single-product.js
+++ web/assets/js/single-product.js
@@ -9,8 +9,17 @@ async function loadData() {
             const json = await response.json();
             if (json.status) {
                 console.log(json);
+                document.getElementById("product-title").innerHTML = json.product.title;
+                document.getElementById("published-on").innerHTML = json.product.created_at;
+                document.getElementById("product-price").innerHTML = json.product.price;
+                document.getElementById("brand").innerHTML = json.product.model.brand.name;
+                document.getElementById("model").innerHTML = json.product.model.name;
+                document.getElementById("condition").innerHTML = json.product.quality.value;
+                document.getElementById("qty").innerHTML = json.product.qty;
             } else {
+
             }
+
         } else {
         }
     } else {
